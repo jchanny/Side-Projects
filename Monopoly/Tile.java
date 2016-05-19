93b72cd8425337135bc7d81a2178f96218ac1bd9
jchanny <jsc126@case.edu>
2016-05-18 21:28:44 -0700

    still learning git

--- Monopoly/Tile.java
+++ Monopoly/Tile.java
@@ -1,21 +1,33 @@
 public class Tile{
 	
+    /**name of the space*/
 	private String name;
 
 	private int houses;
 
 	private int hotels;
 
+    /**cost to add a house*/
 	private final int houseValue;
 
+    /**cost to add a hotel*/
 	private final int hotelValue;
 
-	public Tile(String name, int houseValue, int hotelValue){
+    private final int housePenalty;
+
+    private final int hotelPentaly;
+
+    public Tile(String name, int houseValue, int hotelValue, boolean specialSpace,int housePenalty,
+		int hotelPenalty){
 		this.name=name;
 		houses=0;
 		hotels=0;
-		this.houseValue=houseValue;
-		this.hotelValue=hotelValue;
+		if(specialSpace==true){
+		    this.houseValue=houseValue;
+		    this.hotelValue=hotelValue;
+		}
+		this.housePenalty=housePenalty;
+		this.hotelPenalty=hotelPenalty;
 	}
 
 	public void addHouses(int numHouses){
@@ -34,6 +46,9 @@ public class Tile{
 
 	public void sellHotel(int numHotels){
 		hotels=hotels-numHotels;
-	}
+	}    
 
+    public int fine(){
+	return (housePenalty*numHouses)+(hotelPenalty*numHotels);
+    }
 }
